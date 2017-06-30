c9df7569668f8dfbbe7a7286a07a7aa837318779
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2017-06-30 10:46:20 +0200

    Implemented the real Find The Number game.

---
+++ Software/Microcontroller/Find_The_Number/Includes/Strings.h
@@ -0,0 +1,24 @@
+/** @file String.h
+ * Gather all program strings to ease translation.
+ * @author Adrien RICCIARDI
+ */
+#ifndef H_STRINGS_H
+#define H_STRINGS_H
+
+//-------------------------------------------------------------------------------------------------
+// Constants
+//-------------------------------------------------------------------------------------------------
+#define STRINGS_INSERT_PLAYER_NUMBER "Nombre : "
+#define STRINGS_NUMBER_TOO_SMALL "Trop petit !\n"
+#define STRINGS_NUMBER_TOO_BIG "Trop grand !\n"
+#define STRINGS_PLAYER_WON "Victoire !\n" \
+	"Appuyez sur ENTREE\n" \
+	"pour recommencer ou\n" \
+	"ECHAP pour quitter."
+#define STRINGS_PLAYER_LOST "Perdu...\n" \
+	"Appuyez sur ENTREE\n" \
+	"pour recommencer ou\n" \
+	"ECHAP pour quitter."
+#define STRINGS_REMAINING_ATTEMPTS "Tentatives : "
+
+#endif
--- Software/Microcontroller/Find_The_Number/Sources/Main.c
+++ Software/Microcontroller/Find_The_Number/Sources/Main.c
@@ -2,19 +2,119 @@
  * A simple game using dichotomy to find an unknown number.
  * @author Adrien RICCIARDI
  */
+#include <stdlib.h>
+#include <string.h>
+#include <Strings.h>
 #include <System.h>
 
-void hex(unsigned char val, unsigned char *msb, unsigned char *lsb)
+//-------------------------------------------------------------------------------------------------
+// Private constants
+//-------------------------------------------------------------------------------------------------
+/** How many digits are allowed for the number read from the keyboard. */
+#define NUMBER_MAXIMUM_DIGITS_COUNT 9 // Maximum amount of digits that can be stored on a 4-byte integer.
+
+/** How many times the player can try to win. */
+#define PLAYER_MAXIMUM_ATTEMPTS_COUNT 28 // A computer would need log2(1000000000) = 29.9 attempts to win, make it harder for a human
+
+//-------------------------------------------------------------------------------------------------
+// Private functions
+//-------------------------------------------------------------------------------------------------
+/** Generate the number the player will have to find.
+ * @param String_Number On output, contain the number converted to string. Make sure the buffer is at least NUMBER_MAXIMUM_DIGITS_COUNT + 1 bytes large.
+ */
+static inline void MainGenerateRandomNumber(unsigned char *String_Number)
 {
-	unsigned char i;
+	unsigned char i, Random_Digit;
 	
-	i = val >> 4;
-	if (i < 10) *msb = i + '0';
-	else *msb = i + 'A' - 10;
+	for (i = 0; i < NUMBER_MAXIMUM_DIGITS_COUNT; i++)
+	{
+		Random_Digit = SystemRandomGetNumber() % 10;
+		String_Number[i] = Random_Digit + 48; // Convert number to ASCII representation
+	}
+	// Terminate string
+	String_Number[NUMBER_MAXIMUM_DIGITS_COUNT] = 0;
+}
+
+/** Read a number from the keyboard. Only digits, enter and backspace keys are accepted.
+ * @param String_Number On output, contain the read number converted to a string. Make sure the provided buffer has room for NUMBER_MAXIMUM_DIGITS_COUNT + 1 bytes. The number is aligned on the right and filled with ASCII '0' on the left to allow comparison with the number to find string.
+ */
+static inline void MainReadPlayerNumber(unsigned char *String_Number)
+{
+	unsigned char Character, i = 0, String_Unaligned_Number[NUMBER_MAXIMUM_DIGITS_COUNT + 1], j;
 	
-	i = val & 0x0F;
-	if (i < 10) *lsb = i + '0';
-	else *lsb = i + 'A' - 10;
+	// Display the user prompt
+	SystemDisplayRenderTextString(STRINGS_INSERT_PLAYER_NUMBER);
+	SystemDisplayRenderFrameBuffer();
+	
+	while (1)
+	{
+		Character = SystemKeyboardReadCharacter();
+		
+		switch (Character)
+		{
+			// Backspace
+			case '\b':
+				// There must be at least one character to remove
+				if (i == 0) break;
+
+				// Remove the character from the string
+				String_Unaligned_Number[i] = 0;
+				i--;
+				
+				// Display the backspace to erase the current line last character
+				SystemDisplayRenderTextCharacter('\b');
+				SystemDisplayRenderFrameBuffer();
+				break;
+				
+			// New line
+			case '\n':
+				// Forbid empty string
+				if (i == 0) break;
+				
+				// Add terminating zero
+				String_Unaligned_Number[i] = 0; // 'i' contains the string length
+				
+				// Display the new line character
+				SystemDisplayRenderTextCharacter('\n');
+				SystemDisplayRenderFrameBuffer();
+				
+				// Shift the string to the right and fill leftmost empty digits with ASCII '0' to be able to compare the string with the random number one
+				// Fill number beginning with zeroes
+				for (j = 0; j < NUMBER_MAXIMUM_DIGITS_COUNT - i; j++)
+				{
+					*String_Number = '0';
+					String_Number++;
+				}
+				// Copy number at the string end
+				for (j = 0; j < i; j++)
+				{
+					*String_Number = String_Unaligned_Number[j];
+					String_Number++;
+				}
+				// Terminate string
+				*String_Number = 0;
+					
+				return;
+				
+			// TODO escape
+			
+			default:
+				// Only digits are allowed
+				if ((Character < '0') || (Character > '9')) break;
+				
+				// Make sure the maximum number size is not crossed
+				if (i >= NUMBER_MAXIMUM_DIGITS_COUNT) break;
+				
+				// Append the digit to the string
+				String_Unaligned_Number[i] = Character;
+				i++;
+				
+				// Display the digit
+				SystemDisplayRenderTextCharacter(Character);
+				SystemDisplayRenderFrameBuffer();
+				break;
+		}
+	}
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -22,145 +122,61 @@ void hex(unsigned char val, unsigned char *msb, unsigned char *lsb)
 //-------------------------------------------------------------------------------------------------
 void main(void)
 {
-	unsigned char c, buf1[256], buf2[256], msb, lsb, msg[] = "Message qui doit faire environ 64 carateres pour reussir le test";
-	unsigned short i, j;
+	unsigned char String_Number_To_Find[NUMBER_MAXIMUM_DIGITS_COUNT + 1], String_Player_Number[NUMBER_MAXIMUM_DIGITS_COUNT + 1], Remaining_Attempts = PLAYER_MAXIMUM_ATTEMPTS_COUNT, i;
+	signed char Result;
 	
 	SystemInitialize();
 	
-	// TEST
-	SystemSerialPortWriteString("aaa init string\r\n"); // Sending "aaa" first allows to use UART on RB6 and RB7 pins without being too bored by mikroProg on a Easy PIC 7 MikroElektronika board (value found by chance)
-	SystemSerialPortWriteString("This is a test string\r\n");
-	
-	SystemDisplayClearFrameBuffer();
-	
-	SystemDisplaySetTextCursor(0, 0);
-	SystemDisplayRenderTextString("ABCDEFGHIJKLM");
-	SystemDisplaySetTextCursor(0, 1);
-	SystemDisplayRenderTextString("NOPQRSTUVWXYZ");
-	SystemDisplaySetTextCursor(0, 2);
-	SystemDisplayRenderTextString("0123456789\201");
-	SystemDisplaySetTextCursor(0, 3);
-	SystemDisplayRenderTextString("abcdefghijklm");
-	SystemDisplaySetTextCursor(0, 4);
-	SystemDisplayRenderTextString("nopqrstuvwxyz");
-	SystemDisplaySetTextCursor(0, 5);
-	SystemDisplayRenderTextString("!\"#$%&'()*+,-./");
-	SystemDisplaySetTextCursor(0, 6);
-	SystemDisplayRenderTextString(":;<=>?@[\\]^_`{|}~");
-	
-	SystemDisplayRenderFrameBuffer();
-	
-#if 0
 	while (1)
 	{
-		c = SystemSerialPortReadByte();
+		// Generate the number to find
+		SystemRandomInitialize();
+		MainGenerateRandomNumber(String_Number_To_Find);
 		
-		if ((c >= 'A') && (c <= 'Z')) c += 32;
+		// Clean the display and return cursor to home
+		SystemDisplayClearFrameBuffer();
+		SystemDisplaySetTextCursor(0, 0);
 		
-		SystemSerialPortWriteByte(c);
-	}
-#endif
-
-#if 0
-	for (i = 0; i < 256; i++)
-	{
-		buf1[i] = 255 - (unsigned char) i;
-		buf2[i] = 0;
-	}
-
-	SystemExternalEEPROMWritePage(0, buf1);
-	
-	SystemExternalEEPROMReadPage(0, buf2);
-	
-	for (i = 0; i < 256; i++)
-	{
-		hex(i, &msb, &lsb);
-		SystemSerialPortWriteByte(msb); SystemSerialPortWriteByte(lsb);
-		SystemSerialPortWriteString(" : ");
-		
-		hex(buf2[i], &msb, &lsb);
-		SystemSerialPortWriteByte(msb); SystemSerialPortWriteByte(lsb);
-		SystemSerialPortWriteString("\r\n");
-	}
-#endif
-
-#if 0
-	for (i = 0; i < 256; i++)
-	{
-		buf1[i] = 255 - (unsigned char) i;
-		buf2[i] = (unsigned char) i;
-	}
-
-	SystemExternalEEPROMWritePage(0, buf1);
-	SystemExternalEEPROMWritePage(1, buf2);
-	
-	SystemExternalEEPROMReadPage(0, buf2);
-	for (i = 0; i < 256; i++)
-	{
-		hex(i, &msb, &lsb);
-		SystemSerialPortWriteByte(msb); SystemSerialPortWriteByte(lsb);
-		SystemSerialPortWriteString(" : ");
-		
-		hex(buf2[i], &msb, &lsb);
-		SystemSerialPortWriteByte(msb); SystemSerialPortWriteByte(lsb);
-		SystemSerialPortWriteString("\r\n");
-	}
-	SystemSerialPortWriteString("\r\n");
-	
-	SystemExternalEEPROMReadPage(1, buf2);
-	for (i = 0; i < 256; i++)
-	{
-		hex(i, &msb, &lsb);
-		SystemSerialPortWriteByte(msb); SystemSerialPortWriteByte(lsb);
-		SystemSerialPortWriteString(" : ");
-		
-		hex(buf2[i], &msb, &lsb);
-		SystemSerialPortWriteByte(msb); SystemSerialPortWriteByte(lsb);
-		SystemSerialPortWriteString("\r\n");
-	}
-#endif
-
-#if 0
-	for (j = 0; j < 32768/256; j++) 
-	{
-		SystemExternalEEPROMReadPage(j, buf2);
-		
-		for (i = 0; i < 256; i++)
+		while (1)
 		{
-			hex(i, &msb, &lsb);
-			SystemSerialPortWriteByte(msb); SystemSerialPortWriteByte(lsb);
-			SystemSerialPortWriteString(" : ");
+			MainReadPlayerNumber(String_Player_Number);
+			Remaining_Attempts--;
 			
-			hex(buf2[i], &msb, &lsb);
-			SystemSerialPortWriteByte(msb); SystemSerialPortWriteByte(lsb);
-			SystemSerialPortWriteString("\r\n");
-		}
-		SystemSerialPortWriteString("-------------------------\r\n");
-	}
-	
-#endif
-
-#if 0
-	SystemFlashWriteBlock(0x2000/64, msg);
-#endif
-
-	i = 0;
-	while (1)
-	{
-		if (i == 0)
-		{
-			SystemLedOn();
-			i = 1;
-		}
-		else
-		{
-			SystemLedOff();
-			i = 0;
+			// Is the player number the good one ?
+			Result = strcmp((const char *) String_Player_Number, (const char *) String_Number_To_Find);
+			// Player number is too small
+			if (Result < 0) SystemDisplayRenderTextString(STRINGS_NUMBER_TOO_SMALL);
+			// Player number is too big
+			else if (Result > 0) SystemDisplayRenderTextString(STRINGS_NUMBER_TOO_BIG);
+			// Player found the number
+			else
+			{
+				SystemDisplayRenderTextString(STRINGS_PLAYER_WON);
+				SystemDisplayRenderFrameBuffer();
+				break;
+			}
+			
+			// Display remaining attempts
+			SystemDisplayRenderTextString(STRINGS_REMAINING_ATTEMPTS);
+			SystemDisplayRenderTextString((const unsigned char *) itoa(String_Player_Number, Remaining_Attempts, 10)); // Recycle String_Player_Number
+			SystemDisplayRenderTextCharacter('\n');
+			SystemDisplayRenderFrameBuffer();
+			
+			// Did the player loose ?
+			if (Remaining_Attempts == 0) 
+			{
+				SystemDisplayRenderTextString(STRINGS_PLAYER_LOST);
+				SystemDisplayRenderFrameBuffer();
+				break;
+			}
 		}
 		
-		__delay_ms(458);
+		// Wait for the player choice to start a new game or to quit
+		while (1)
+		{
+			i = SystemKeyboardReadCharacter(); // Recycle 'i' variable
+			if (i == '\n') break;
+			// TODO escape
+		}
 	}
-	
-	// TEST
-	while (1);
 }
