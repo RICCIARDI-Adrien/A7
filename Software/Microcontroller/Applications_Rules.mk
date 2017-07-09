b467b2db29d295a7e9e4ca25b831df4050615fe5
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2017-07-09 14:01:21 +0200

    Moved Applications_Rules.mk rules into the main makefile.

--- Software/Microcontroller/Applications_Rules.mk
+++
@@ -1,5 +0,0 @@
-# Gather variables shared by all applications.
-export APPLICATIONS_RULES_PATH_BINARIES = ../../Binaries
-
-export APPLICATIONS_RULES_C_COMPILER = xc8
-export APPLICATIONS_RULES_C_COMPILER_FLAGS = --chip=18F26K22 -I../System/Includes -I$(XC8_INCLUDES_PATH) $(APPLICATIONS_RULES_PATH_BINARIES)/System.lpp --codeoffset=0x2000
