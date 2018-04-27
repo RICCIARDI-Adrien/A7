9917b82421fbe1fbcfa65125a2ba1a3e66544012
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2018-04-27 23:02:06 +0200

    Added a main menu to Benchmark application.

---
+++ Software/Microcontroller/Benchmark/Includes/Strings.h
@@ -0,0 +1,15 @@
+/** @file Strings.h
+ * Gather all program strings to ease translation.
+ * @author Adrien RICCIARDI
+ */
+#ifndef H_STRINGS_H
+#define H_STRINGS_H
+
+//-------------------------------------------------------------------------------------------------
+// Constants
+//-------------------------------------------------------------------------------------------------
+#define STRINGS_MENU_TITLE " - Choix benchmark -\n\n"
+#define STRINGS_MENU_FIRST_CHOICE "1. Balles\n"
+#define STRINGS_MENU_SECOND_CHOICE "2. Calculs flottants\n"
+
+#endif
--- Software/Microcontroller/Benchmark/Sources/Main.c
+++ Software/Microcontroller/Benchmark/Sources/Main.c
@@ -3,7 +3,33 @@
  * @author Adrien RICCIARDI
  */
 #include <Benchmark.h>
+#include <Strings.h>
 #include <System.h>
+
+//-------------------------------------------------------------------------------------------------
+// Private functions
+//-------------------------------------------------------------------------------------------------
+/** Display a menu and wait for the user to choose one item.
+ * @return The chosen item (starting from 0, so item #1 will return 0).
+ */
+static inline unsigned char MainShowMenu(void)
+{
+	unsigned char Key;
+	
+	// Display menu
+	SystemDisplayRenderTextString(STRINGS_MENU_TITLE);
+	SystemDisplayRenderTextString(STRINGS_MENU_FIRST_CHOICE);
+	SystemDisplayRenderTextString(STRINGS_MENU_SECOND_CHOICE);
+	SystemDisplayRenderFrameBuffer();
+	
+	// Wait for the user to press a valid key
+	do
+	{
+		Key = SystemKeyboardReadCharacter();
+	} while ((Key < '1') || (Key > '2'));
+	
+	return Key - '1';
+}
 
 //-------------------------------------------------------------------------------------------------
 // Entry point
@@ -12,5 +38,16 @@ void main(void)
 {
 	SystemInitialize();
 	
-	BenchmarkBouncingBalls();
+	// Start the selected benchmark
+	switch (MainShowMenu())
+	{
+		case 0:
+			BenchmarkBouncingBalls();
+			break;
+			
+		default:
+			break;
+	}
+	
+	SystemExitProgram();
 }
